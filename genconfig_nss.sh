8c360d5d0ecdbb7360df390f3fea3ff4df306fd0
Chris Hozian <chozian@gmail.com>
2024-10-06 16:09:01 -0500

    Add personal preferences to .config for mesh full
    CONFIG_PACKAGE_block-mount=y
    CONFIG_PACKAGE_batctl-default=y
    CONFIG_PACKAGE_kmod-batman-adv=y
    CONFIG_BATMAN_ADV_BATMAN_V=y
    CONFIG_BATMAN_ADV_BLA=y
    CONFIG_BATMAN_ADV_DAT=y
    CONFIG_BATMAN_ADV_NC=y
    CONFIG_BATMAN_ADV_MCAST=y
    CONFIG_PACKAGE_luci-proto-batman-adv=y
    CONFIG_PACKAGE_luci-app-sqm=y
    CONFIG_PACKAGE_luci-app-watchcat=y
    CONFIG_PACKAGE_collectd-mod-thermal=y
    CONFIG_PACKAGE_luci-app-statistics=y
    CONFIG_PACKAGE_kmod-nf-conntrack-netlink=y
    CONFIG_PACKAGE_luci-app-nlbwmon=y
    CONFIG_PACKAGE_px5g-standalone=y
    CONFIG_PACKAGE_luci-app-uhttpd=y
    CONFIG_PACKAGE_luci-app-advanced-reboot=y

--- genconfig_nss.sh
+++ genconfig_nss.sh
@@ -6,6 +6,24 @@ cat nss-setup/config-nss.seed |  grep -v CONFIG_PACKAGE_luci >> .config
 echo "
 CONFIG_TARGET_qualcommax_ipq807x_DEVICE_linksys_mx4300=y
 CONFIG_PACKAGE_luci=y
+CONFIG_PACKAGE_block-mount=y
+CONFIG_PACKAGE_batctl-default=y
+CONFIG_PACKAGE_kmod-batman-adv=y
+CONFIG_BATMAN_ADV_BATMAN_V=y
+CONFIG_BATMAN_ADV_BLA=y
+CONFIG_BATMAN_ADV_DAT=y
+CONFIG_BATMAN_ADV_NC=y
+CONFIG_BATMAN_ADV_MCAST=y
+CONFIG_PACKAGE_luci-proto-batman-adv=y
+CONFIG_PACKAGE_luci-app-sqm=y
+CONFIG_PACKAGE_luci-app-watchcat=y
+CONFIG_PACKAGE_collectd-mod-thermal=y
+CONFIG_PACKAGE_luci-app-statistics=y
+CONFIG_PACKAGE_kmod-nf-conntrack-netlink=y
+CONFIG_PACKAGE_luci-app-nlbwmon=y
+CONFIG_PACKAGE_px5g-standalone=y
+CONFIG_PACKAGE_luci-app-uhttpd=y
+CONFIG_PACKAGE_luci-app-advanced-reboot=y
 CONFIG_FEED_nss_packages=n" >> .config
 make defconfig
 
